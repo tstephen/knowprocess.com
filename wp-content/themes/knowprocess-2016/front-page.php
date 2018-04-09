9aa57e95c2b45ef01e0943230e7b82b1edf8a695
Gitium <gitium@presslabs.com>
2018-04-09 17:07:47 +0000

    update home page

--- wp-content/themes/knowprocess-2016/front-page.php
+++ wp-content/themes/knowprocess-2016/front-page.php
@@ -5,17 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Know Process - we do!</title>
-    <link rel="icon" type="image/png" href="/img/icon/brain-gears-16x16.png" />
-    <link rel="shortcut icon" type="image/png" href="/img/icon/brain-gears-16x16.png">
-
-    <!-- Icon font -->
+    <link rel="icon" type="image/png" href="https://api.knowprocess.com/images/icon/knowprocess-icon-16x16.png">
+    <link rel="shortcut icon" type="image/png" href="https://api.knowprocess.com/images/icon/knowprocess-icon-16x16.png">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri () ?>/css/icons/style.css">
-    
-    <!-- Bootstrap -->
     <link href="<?php echo get_template_directory_uri () ?>/css/bootstrap.min.css" rel="stylesheet">
-
     <link href="<?php echo get_template_directory_uri () ?>/css/front-page.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Inder|Galdeano' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,126 +22,79 @@
 
     <div class="container">
 
-      <div class="row hidden-sm hidden-xs">
+      <div class="row ">
 
-            <div class="tile col-md-2 col-lg-3">
+            <div class="tile hidden-xs col-sm-2 col-md-2 col-lg-3">
               <h2></h2>
               <p> </p>
             </div><!--/span-->
-            <div class="trakeo-tile tile col-md-4 col-lg-3">
-              <h2><img style="left:auto;right:auto;height:60px;" src="<?php echo get_template_directory_uri () ?>/images/trakeo-logo-185x70.png"/></h2>
+            <div class="srp-tile tile col-xs-6 col-sm-4 col-md-4 col-lg-3">
+              <h2 class="has-img">Sustainable Resource Planning</h2>
               <h3>Move from mere <em>sustainable policy</em> to firm <em>sustainable practice</em></h3>
-              <p><a class="btn btn-default" href="http://www.trakeo.com/" role="button">Learn more &raquo;</a></p>
+              <p class="tile-bottom"><a class="btn btn-default" href="//srp.digital/" role="button">Learn more &raquo;</a></p>
             </div><!--/span-->
-            <div class="tile col-md-2 col-lg-3">
+            <div class="tile hidden-xs col-sm-2 col-md-2 col-lg-3">
               <h2></h2>
               <p></p>
             </div><!--/span-->
-            <div class="knowprocess-tile tile col-md-4 col-lg-3">
-              <h2>Get to Know Process</h2>
-              <p>
-                <ul style="list-style:none">
-                  <li><a href="mailto:tim@knowprocess.com" class="glyphicon glyphicon-envelope btn">tim@knowprocess.com</a></li>
-                  <li><a href="http://www.twitter.com/knowprocess" class="kp-icon-twitter kp-glyph btn">@tstephen10</a></li>
-                  <li><a href="/blog" class="kp-icon-rss kp-glyph btn">Blog</a></li>
-                <li><a href="http://www.linkedin.com/company/2819688?trk=tyah" class="kp-icon-linkedin kp-glyph btn"></a></li>
-                </ul>   
-              </p>
+            <div class="knowprocess-tile tile col-xs-6 col-sm-4 col-md-4 col-lg-3">
+              <h2 class="has-img"><img src="https://api.knowprocess.com/images/knowprocess-logo.png" style="height:100%;"/></h2>
+              <h3>&nbsp;</h3>
+              <p style="margin-top: 20px;"><a class="btn btn-default" href="https://cloud.knowprocess.com" role="button">Application Login &raquo;</a></p>
+              <p class="tile-bottom"><a class="btn" href="/reset-password">Forgotten your password? &raquo;</a></p>
+
             </div><!--/span-->
-            <div class="omny-tile tile col-md-4 col-lg-3">
+            <div class="omny-tile tile col-xs-6 col-sm-4 col-md-4 col-lg-3">
               <h2><img src="<?php echo get_template_directory_uri () ?>/images/omny-logo.png"/></h2>
               <h3>We <span class="omny-emphasis">automate</span> your <br>
 <span class="omny-emphasis">digital</span> channels<br/>
               </h3>
-              <p style="margin-top: 40px;"><a class="btn btn-default" href="http://omny.link" role="button">Learn more &raquo;</a></p>
+              <p class="tile-bottom"><a class="btn btn-default" href="http://omny.link" role="button">Learn more &raquo;</a></p>
             </div><!--/span-->
-            <div class="col-md-2 col-lg-3">
+            <div class="col-sm-2 col-md-2 col-lg-3">
               <h2></h2>
               <p></p>
             </div><!--/span-->
-            <div class="knowprocess-tile tile col-md-4 col-lg-3">
-              <h2 class="has-img" style="padding-left:35px"><img src="<?php echo get_template_directory_uri () ?>/images/bpmn-miwg.png"/></h2>
+            <div class="knowprocess-inverse-tile tile col-xs-6 col-sm-4 col-md-4 col-lg-3">
+              <h2 class="has-img"><img src="<?php echo get_template_directory_uri () ?>/images/bpmn-miwg.png"/></h2>
               <h3>Improving the state of Business Process model interoperability</h3>
-              <p>
+              <p class="tile-bottom">
                 <!--<img src="http://www.omgwiki.org/bpmn-miwg/lib/exe/fetch.php?w=200&media=miwg-logo-827x512.png"/>-->
                 <a class="btn btn-default" href="http://bpmn-miwg.github.io/bpmn-miwg-tools/" role="button">Learn more &raquo;</a></p>
             </div><!--/span-->
-            <div class="tile tile col-md-2 col-lg-3">
+            <div class="tile hidden-xs col-sm-2 col-md-2 col-lg-3">
               <h2></h2>
               <p></p>
             </div><!--/span-->
-            <div class="tile col-md-2 col-lg-3">
+            <div class="tile hidden-xs col-sm-2 col-md-2 col-lg-3">
               <h2></h2>
             </div><!--/span-->
-            <div class="onedecision-tile tile col-md-4 col-lg-3">
-              <h2 class="has-img" style="padding-left:70px">
-                <img style="height: 120%;top: -10%;position: relative;" src="<?php echo get_template_directory_uri () ?>/images/one-decision-logo-200x90.png"/>
+            <div class="onedecision-tile tile col-xs-6 col-sm-4 col-md-4 col-lg-3">
+              <h2 class="has-img">
+                <img style="height: 120%;" src="<?php echo get_template_directory_uri () ?>/images/one-decision-logo-200x90.png"/>
               </h2>
               <h3>Open Source Decision Management supporting DMN</h3>
-              <p><a class="btn btn-default" href="http://onedecision.io" role="button" target="_newtab">Learn more &raquo;</a></p>
+              <p class="tile-bottom"><a class="btn btn-default" href="http://onedecision.io" role="button" target="_newtab">Learn more &raquo;</a></p>
             </div><!--/span-->
-            <div class="tile col-md-2 col-lg-3">
+            <div class="tile hidden-xs col-sm-2 col-md-2 col-lg-3">
               <h2></h2>
               <p> </p>
             </div><!--/span-->
-            <div class="knowprocess-tile tile col-md-4 col-lg-3">
+            <div class="knowprocess-tile tile col-xs-6 col-sm-4 col-md-4 col-lg-3">
               <h2></h2>
               <p><img class="img-rounded" src="<?php echo get_template_directory_uri () ?>/images/profile-tim-200x200.jpg" style="width:100%;padding-left:15%;padding-right:15%;"/></p>
-              <p style="height:100%;text-align:center;vertical-align:bottom;">&copy; Tim Stephenson 2014-2016</p>
+              <p class="tile-bottom" style="text-align: center">&copy; Tim Stephenson 2014-2018</p>
             </div><!--/span-->
-<div class="knowprocess-tile tile col-md-4 col-lg-3">
-              <h2 class="has-img" style="padding-left:60px"><img src="<?php echo get_template_directory_uri () ?>/images/bpsim.gif"/></h2>
+<div class="knowprocess-tile tile col-xs-6 col-sm-4 col-md-4 col-lg-3">
+              <h2 class="has-img"><img src="<?php echo get_template_directory_uri () ?>/images/bpsim.gif"/></h2>
               <h3>A standard for exchanging information on simulating business processes</h3>
-              <p><a class="btn btn-default" href="http://www.bpsim.org" role="button" target="_newtab">Learn more &raquo;</a></p>
+              <p class="tile-bottom"><a class="btn btn-default" href="http://www.bpsim.org" role="button" target="_newtab">Learn more &raquo;</a></p>
             </div><!--/span-->
-            <div class="tile col-md-2 col-lg-3">
+            <div class="tile hidden-xs col-sm-2 col-md-2 col-lg-3">
               <h2></h2>
               <p> </p>
             </div><!--/span-->
-            <div class="syncapt-tile tile col-md-4 col-lg-3">
-              <h2><img src="<?php echo get_template_directory_uri () ?>/images/syncapt-logo.png"/></h2>
-              <h3>We <span class="syncapt-emphasis">sync</span> your preferred<br>
-cloud <span class="syncapt-emphasis">software</span> to boost<br>
-your <span class="syncapt-emphasis">performance</span> 
-              </h3>
-              <p><a class="btn btn-default" href="http://www.syncapt.com" role="button">Learn more &raquo;</a></p>
-            </div><!--/span-->
-          </div><!--/row-->
-
-      <div class="row hidden-md hidden-lg">
-
-            <div class="knowprocess-tile tile col-xs-12 col-sm-6 col-md-3 col-lg-3">
-              <h2></h2>
-              <p><img class="img-rounded" src="<?php echo get_template_directory_uri () ?>/images/profile-tim-200x200.jpg" style="width:100%;padding-left:15%;padding-right:15%;"/></p>
-              <p style="height:100%;text-align:center;vertical-align:bottom;">&copy; Tim Stephenson 2014-2016</p>
-            </div><!--/span-->
-            <div class="trakeo-tile tile col-xs-12 col-sm-6">
-              <h2><img style="left:auto;right:auto;height:60px;" src="<?php echo get_template_directory_uri () ?>/images/trakeo-logo-185x70.png"/></h2>
-<!-- http://trakeo.com/wp-content/uploads/2013/08/Puffin2.jpg -->
-              <h3>Move from mere <em>sustainable policy</em> to firm <em>sustainable practice</em></h3>
-              <p><a class="btn btn-default" href="http://www.trakeo.com/" role="button">Learn more &raquo;</a></p>
-            </div><!--/span-->
-            <div class="syncapt-tile tile col-xs-12 col-sm-6">
-              <h2><img style="height:45px;" src="<?php echo get_template_directory_uri () ?>/images/syncapt-logo.png"/></h2>
-              <h3>We <span class="syncapt-emphasis">sync</span> your preferred<br>
-cloud <span class="syncapt-emphasis">software</span> to boost<br>
-your <span class="syncapt-emphasis">performance</span> 
-              </h3>
-              <p><a class="btn btn-default" href="http://www.syncapt.com" role="button">Learn more &raquo;</a></p>
-            </div><!--/span-->
-            <div class="knowprocess-inverse-tile tile col-xs-12 col-sm-6">
-              <h2 class="has-img" style="padding-left:35px"><img src="<?php echo get_template_directory_uri () ?>/images/bpmn-miwg.png"/></h2>
-              <h3>Improving the state of Business Process model interoperability</h3>
-              <p>
-                <!--<img src="http://www.omgwiki.org/bpmn-miwg/lib/exe/fetch.php?w=200&media=miwg-logo-827x512.png"/>-->
-                <a class="btn btn-default" href="http://bpmn-miwg.github.io/bpmn-miwg-tools/bpmn-tool-list/bpmn-tools.html" role="button">Learn more &raquo;</a></p>
-            </div><!--/span-->
-            <div class="knowprocess-inverse-tile tile col-xs-12 col-sm-6">
-              <h2 class="has-img" style="padding-left:60px"><img src="<?php echo get_template_directory_uri () ?>/images/bpsim.gif"/></h2>
-              <h3>A standard for exchanging information on simulating business processes</h3>
-              <p><a class="btn btn-default" href="http://www.bpsim.org" role="button" target="_newtab">Learn more &raquo;</a></p>
-            </div><!--/span-->
-            <div class="knowprocess-tile tile col-xs-12 col-sm-6">
+            <div class="knowprocess-inverse-tile tile col-xs-6 col-sm-4 col-md-4 col-lg-3">
               <h2>Get to Know Process</h2>
               <p>
                 <ul style="list-style:none">
@@ -160,9 +107,6 @@ your <span class="syncapt-emphasis">performance</span>
             </div><!--/span-->
           </div><!--/row-->
 
-        </div><!--/span-->
-
-    </div><!--/.container-->
 
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
