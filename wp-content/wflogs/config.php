41ada4b0313f37847258fe23e4eb80ba0cab1402
tim@knowprocess.com <tim@knowprocess.com>
2018-08-06 20:00:44 +0000

    modified file `wflogs` (after activation of `Wordfence Security` version 7.1.10)

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1534190439;s:7:"authKey";s:64:"$~b!)tStQvQNNbWy4: 5A_8Sl95uKc3wWBA@U0IJ</5hcKL+/3Lkeq!l/braOT^7";}
